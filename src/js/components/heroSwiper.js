700576e43d718f9baef501c11f9c69782805d527
Aliaksandr <djvalentinoff@gmail.com>
2022-10-28 17:19:55 +0300

    feat: add setings swiper hero

--- src/js/components/heroSwiper.js
+++ src/js/components/heroSwiper.js
@@ -1,9 +1,15 @@
-import Swiper, { Pagination, Autoplay, Keyboard, A11y, Navigation, Grid } from 'swiper';
-Swiper.use([Pagination, Autoplay, Keyboard, A11y, Navigation, Grid]);
+import Swiper, { Pagination, Autoplay, Keyboard, A11y, Navigation, EffectFade } from 'swiper';
+Swiper.use([Pagination, Autoplay, Keyboard, A11y, Navigation, EffectFade]);
 
 const heroSwiper = document.querySelector('.hero-swiper');
 if (heroSwiper) {
   heroSwiper = new Swiper('.hero-swiper', {
-
+    allowTouchMove: false,
+    loop: true,
+    effect: 'fade',
+    speed: 8000,
+    autoplay: {
+      delay: 5000
+    }
   });
 }
